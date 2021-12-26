ef48166db4e2d90fdf5f93656fa2a8bc13e0d443
HJinS <windowed@naver.com>
2021-12-26 23:42:25 +0900

    2021-12-26 정규식 테스트중 수정할 것

--- amazonCrawler.py
+++ amazonCrawler.py
@@ -2,6 +2,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.expected_conditions import presence_of_all_elements_located, presence_of_element_located
 from crawler import Crawler
+from collections import deque
+from resultFilter import ResultFilter
+
 
 """
 상품 id 가져올 것
@@ -14,6 +17,7 @@ class AmazonCrawler(Crawler):
     def __init__(self, driver_path: str, keywords: list):
         super().__init__("https://www.amazon.com/", driver_path)
         self.keywords = keywords
+        self.resultQueue = deque()
 
     def __get_data(self, keyword):
         self.driver.get(self.site_loc)
@@ -25,17 +29,19 @@ class AmazonCrawler(Crawler):
             total_pagination_num = int(list(self.driver.find_element(By.CLASS_NAME,'a-pagination').text.split('\n'))[-2])
         except:
             total_pagination_num = int(self.driver.find_elements(By.CLASS_NAME, 's-pagination-item')[-2].text)
-            
-        for page in range(total_pagination_num):    
-            self.__get_page_data()
+        print("total page = ", total_pagination_num)
+        for page in range(min(1, total_pagination_num)):
+            print("page ", page, "--------------------", end='')
+            self.__get_page_data(keyword)
             self.driverWait.until(presence_of_all_elements_located((By.XPATH, '//*[@id="search"]/div[1]/div[1]/div/span[3]/div[2]')))
             try:
                 next_btn = self.driver.find_element(By.CLASS_NAME,'a-pagination').find_element(By.CLASS_NAME, 'a-last')
             except:
                 next_btn = self.driver.find_elements(By.CLASS_NAME, 's-pagination-item')[-1]
             next_btn.click()
+            print("complete")
 
-    def __get_item_data(self, item):
+    def __get_item_data(self, item, keyword):
         class_name = item.get_attribute('class')
         if "AdHolder" in class_name:
             return
@@ -50,24 +56,22 @@ class AmazonCrawler(Crawler):
         
         price_list_unit = list(str(price.text).split('\n'))
         price_str = price_list_unit[0] + '.' + price_list_unit[1]
-        print("-----------------------")
-        print("id = ", product_id)
-        print(item.text)
-        print(img)
-        print(product_name)
-        print("price = ", price_str)
-        print("-----------------------")
+        self.resultQueue.append([product_id, img, product_name, price_str, keyword])
         
-    def __get_page_data(self):
+    def __get_page_data(self, keyword):
         data = self.driver.find_element(By.XPATH, '//*[@id="search"]/div[1]/div[1]/div/span[3]/div[2]')
         data_list = data.find_elements(By.XPATH, '//div[@data-component-type="s-search-result"]')
         for item in data_list:
-            self.__get_item_data(item)
+            self.__get_item_data(item, keyword)
 
     def save(self):
         for keyword in self.keywords:
             self.__get_data(keyword=keyword)
             
-
-crawler = AmazonCrawler("E:\Study\chromedriver.exe", ["12900k"])
-crawler.save()
+        return self.resultQueue
+            
+keywords = ["cpu"]
+crawler = AmazonCrawler("E:\Study\chromedriver.exe", keywords)
+test_q = crawler.save()
+filter = ResultFilter(test_q, keywords)
+filter.filtering()
--- resultFilter.py
+++ resultFilter.py
@@ -2,16 +2,31 @@ import re
 from collections import deque
 
 class ResultFilter:
-    def __init__(self, resultQueue: deque, search_keyword: str):
+    def __init__(self, resultQueue: deque, search_keyword: list()):
         self.result = resultQueue
         self.searchKeyword = search_keyword
+        self.reInit()
         
     def reInit(self):
-        self.re = {
-            'cpu' : "r'(intel|amd).*?(processor|프로세서)(?!system|시스템|pc)\b'",
+        self.expression = {
+            'cpu' : "r'[Ii][Nn][Tt][Ee][Ll]|[Aa][Mm][Dd]|인텔.*?[Pp][Rr][Oo][Cc][Ee][Ss][Ss][Oo][Rr]|프로세서.*?(?![Ss][Yy][Ss][Tt][Ee][Mm]|시스템|[Pp][Cc])'",
             'gpu' : "r'(nvdia|radeon).*?(gpu|graphics? cards?|video card|그래픽 카드)(?!system|시스템|pc)'",
             'ram' : "r'ddr4.*?(ram|메모리)(?!system|시스템|pc)'",
             'ssd' : "r'ssd.*?(nvme|sata)(?!system|시스템|pc)",
-            'cooler' : "r'(liquid cooler|cpu cooler).*'",
+            'cpu cooler' : "r'(liquid cooler|cpu cooler).*'",
         }
         
+    def filtering(self):
+        f = open("result.txt", 'w', encoding="UTF-8")
+        for result_item in self.result:
+            id, image, name, price, keyword = result_item
+            self.re = re.compile(self.expression[keyword])
+            match_result = self.re.findall(name)
+            if len(match_result) == 0:
+                print(name)
+                print("부적합\n")
+            else:
+                f.write(id + " "  + image + " " + price + " " + keyword + "\n")
+                f.write(name + "\n")
+                ##print(match_result.group(1))
+        f.close()
