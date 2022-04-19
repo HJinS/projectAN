d3e168e7dfd6b1647fcec1ba87c93ea59af474fb
HJinS <windowed@naver.com>
2022-04-19 23:06:42 +0900

    2022-04-19 celery container 테스트

--- projectAN/projectANConfig/Celery.py
+++ projectAN/projectANConfig/Celery.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectANConfig.settings')
 HOST = os.environ.get('SERVER_HOST')
-app = Celery('projectAN', broker=f'amqp://guest:guest@{HOST}:5672//')
+app = Celery('projectAN', broker=f'amqp://guest:guest@{HOST}')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
